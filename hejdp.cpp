b961b224e48af264b175fd1add512e322145aa9b
e-almroth <123807247+e-almroth@users.noreply.github.com>
2024-01-25 13:46:13 +0100

    Create hejdp.cpp

---
+++ hejdp.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string svar;
+    cout << "hej! what is your response?: " << endl;
+    cin >> svar;
+
+    for(int x = 100; x > 0; x--){
+        cout << svar << endl;
+    }
+
+    return 0;
+}
