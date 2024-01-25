c36d67337cef3de810bc2b0be226db0fd6a6fdb2
IsakLundahl <157359136+IsakLundahl@users.noreply.github.com>
2024-01-25 13:51:57 +0100

    Update hejdp.cpp

--- hejdp.cpp
+++ hejdp.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <string>  // Make sure to include this for the string class
 using namespace std;
 
 int main() {
     string response;
     cout << "Enter your response: ";
-    cin >> response;
+    getline(cin, response); // Using getline to read the whole line
 
-    for (int x = 0; x <= 100; x++) {
+    for (int x = 0; x < 100; x++) {
         cout << response << endl;
     }
 
