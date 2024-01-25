9db3d4002de765f5e060d7c5a072fef3666379e7
e-almroth <123807247+e-almroth@users.noreply.github.com>
2024-01-25 13:52:41 +0100

    Update hejdp.cpp

--- hejdp.cpp
+++ hejdp.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 int main() {
     string response;
-    cout << "Enter your response: ";
+    cout << "Fuck you! Enter your response: ";
     getline(cin, response); // Using getline to read the whole line
 
-    for (int x = 0; x < 100; x++) {
+    for (int x = 0; x <= 100; x++) {
         cout << response << endl;
     }
 
